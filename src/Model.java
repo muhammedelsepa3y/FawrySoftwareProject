1d32287be5a169090e844df789decf5cdf69be99
MohamedEssam <mo849026@gmail.com>
2022-12-01 18:48:24 +0200

    Add Model and UserModel

---
+++ src/Model.java
@@ -0,0 +1,23 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Model {
+
+    private static List<UserModel> users= new ArrayList<UserModel>();
+    private static List<UserModel> admins= new ArrayList<UserModel>();
+
+    public static void AddUser(UserModel user) {
+        users.add(user);
+    }
+    public static void AddAdmin(UserModel admin) {
+        admins.add(admin);
+    }
+
+    public static List<UserModel> getUsers() {
+        return users;
+    }
+
+    public static List<UserModel> getAdmins() {
+        return admins;
+    }
+}
---
+++ src/UserModel.java
@@ -0,0 +1,26 @@
+public class UserModel {
+
+    private String name;
+    private boolean isAdmin;
+    private String email;
+    private String password;
+    private String phone;
+    private String username;
+
+    public UserModel(String name, String email, String password, String phone, String username, boolean isAdmin) {
+        this.name = name;
+        this.email = email;
+        this.password = password;
+        this.phone = phone;
+        this.username = username;
+        this.isAdmin = isAdmin;
+    }
+    public String getEmail() {
+        return email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+}
