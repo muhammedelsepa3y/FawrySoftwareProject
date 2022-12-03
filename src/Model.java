793f00faa65565aa29a4a20e0b3abe0f34dfa68e
MohamedEssam <mo849026@gmail.com>
2022-12-03 20:39:03 +0200

    update in Model class

--- src/Model.java
+++ src/Model.java
@@ -6,11 +6,17 @@ public class Model {
     private static List<UserModel> users= new ArrayList<UserModel>();
     private static List<UserModel> admins= new ArrayList<UserModel>();
 
+    private static List<DiscountModel> discounts= new ArrayList<DiscountModel>();
+
     public static void AddUser(UserModel user) {
         users.add(user);
     }
     public static void AddAdmin(UserModel admin) {
         admins.add(admin);
+    }
+
+    public static void AddDiscount(DiscountModel discount) {
+        discounts.add(discount);
     }
 
     public static List<UserModel> getUsers() {
@@ -21,4 +27,9 @@ public class Model {
         return admins;
     }
 
+    public static List<DiscountModel> getDiscounts() {
+        return discounts;
+    }
+
+    public static void RemoveDiscount(DiscountModel discount) {discounts.remove(discount);}
 }
