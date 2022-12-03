ba2eed5f7309143ec92391fa3a65c0d5a53f4583
Mohamed Elsebaey <muhammedelsepa3y@gmail.com>
2022-12-03 02:50:01 +0200

    add default admin and user

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,8 @@
 
 public class Main {
     public static void main(String[] args) {
+        Model.AddAdmin(new UserModel("admin","admin@admin.com","admin","01000000000","admin",true));
+        Model.AddUser(new UserModel("user","user@user.com","user","01000000000","user",false));
         RootOfTheApplication.Start();
     }
 }
