7b93cc86e728bb821d203f783cf7ce3a6b6938d7
Mohamed Elsebaey <muhammedelsepa3y@gmail.com>
2022-12-03 19:13:00 +0200

    add wallet to user update user Model

--- src/UserModel.java
+++ src/UserModel.java
@@ -1,12 +1,12 @@
 public class UserModel {
 
-    private String name;
-    private boolean isAdmin;
-    private String email;
-    private String password;
-    private String phone;
-    private String username;
-
+    private  String name;
+    private  boolean isAdmin;
+    private  String email;
+    private  String password;
+    private  String phone;
+    private  String username;
+    private  int wallet=0;
     public UserModel(String name, String email, String password, String phone, String username, boolean isAdmin) {
         this.name = name;
         this.email = email;
@@ -29,4 +29,13 @@ public class UserModel {
         return name;
     }
 
+    public int addWallet(int amount) {
+        return wallet+=amount;
+    }
+    public int getWallet() {
+        return wallet;
+    }
+    public int deductWallet(int amount) {
+        return wallet-=amount;
+    }
 }
