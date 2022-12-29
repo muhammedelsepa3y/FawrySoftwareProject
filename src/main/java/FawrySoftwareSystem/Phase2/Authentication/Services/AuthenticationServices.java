ca1425f20f4985c0e3f3ad95a9efce9cf8928d09
Malak Emad <119331267+malakemadd@users.noreply.github.com>
2022-12-30 01:44:47 +0200

    some updates

---
+++ src/main/java/FawrySoftwareSystem/Phase2/Authentication/Services/AuthenticationServices.java
@@ -0,0 +1,55 @@
+package com.fci.advanced.sw.fawrysoftwaresystem.authentication.Services;
+
+import com.fci.advanced.sw.fawrysoftwaresystem.authentication.Repository.AuthRepository;
+import com.fci.advanced.sw.fawrysoftwaresystem.authentication.Model.UserModel;
+import org.springframework.stereotype.Service;
+
+import java.util.UUID;
+
+@Service
+public class AuthenticationServices {
+    private final AuthRepository authRepository;
+    public AuthenticationServices(AuthRepository authRepository) {
+        this.authRepository = authRepository;
+    }
+
+    public String UserRegister(UserModel user) {
+        if (authRepository.userExistBefore(user)) {
+            return "User Registered Successfully";
+        } else {
+            return "User Already Exists";
+        }
+    }
+
+    public String UserLogin(String email, String password) {
+        UUID id = authRepository.userRegistered(email, password);
+        String Response = "";
+        if (id != null) {
+            Response = "User Logged In Successfully\n";
+            Response+= "User Id: " + id;
+            return Response;
+
+        } else {
+            Response = "User Not Found";
+            return Response;
+        }
+    }
+
+    public String UserLogout(UUID id) {
+        if(authRepository.userLogin(id)) {
+            return "User Logged Out Successfully";
+        } else {
+            return "User Not Found Or Not Logged In";
+        }
+    }
+
+    public Object CurrentUser(UUID id) {
+        UserModel user = authRepository.userDetails(id);
+        if(user != null) {
+            return user;
+        } else {
+            return "User Not Found Or Not Logged In";
+        }
+    }
+
+}
