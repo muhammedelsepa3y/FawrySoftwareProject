37ef59c88ad3365e264f300a1aa6bc3654b00d61
MohamedEssam <mo849026@gmail.com>
2023-01-01 02:11:45 +0200

    add Discount Model

---
+++ src/main/java/FawrySoftwareSystem/Phase2/Models/DiscountModel.java
@@ -0,0 +1,32 @@
+package FawrySoftwareSystem.Phase2.Models;
+
+import java.util.Date;
+
+public class DiscountModel {
+    private String FeatureName;
+    private int discountPercentage;
+    private String discountDescription;
+    private boolean isOverAll;
+    private int discountID;
+    public DiscountModel(String FeatureName, int discountPercentage) {
+        this.FeatureName = FeatureName;
+        this.discountPercentage = discountPercentage;
+        this.discountDescription = new Date().toString();
+        this.discountID = (int) (Math.random() * 1000000);
+    }
+
+    public String getFeatureName() {
+        return FeatureName;
+    }
+    public int getDiscountPercentage() {
+        return discountPercentage;
+    }
+    public String getDiscountDescription() {
+        return discountDescription;
+    }
+    public int getDiscountID() {
+        return discountID;
+    }
+
+
+}
